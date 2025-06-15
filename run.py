ad27e5499807d2191eb713b82eddca6cd863a04b
ates111 <tandamata369@gmail.com>
2025-06-15 09:30:37 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+.
+├── run.py                  ← isi dengan script Python kamu
+├── requirements.txt        ← semua dependency Python (lihat di bawah)
+└── .github
+    └── workflows
+        └── main.yml        ← GitHub Actions untuk menjalankan run.py
